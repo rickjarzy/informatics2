d8aea658b1a449fb33b345697bfad904787dbf80
Arzberger Paul <Paul.Arzberger@xal.com>
2019-03-09 11:37:21 +0100

    finished task 1

--- ex01Signals/ex01Signals.py
+++ ex01Signals/ex01Signals.py
@@ -1,16 +1,16 @@
 # Paul Arzberger
 # 00311430
 # Informatics 2 - SS19
-
+# Gruppe A
 import numpy
 from matplotlib import pyplot as plt
 
 def readSignals(file_path):
-
+    """Reads out the signals.txt and writes the content on a dict"""
     # open file with context manager
     with open(file_path) as file:
 
-        return_dict = {}
+        return_dict = {}                                # allocate dict
 
         for line in file.readlines():
 
@@ -19,37 +19,52 @@ def readSignals(file_path):
             #pint(parts[1].split(","))                      #split right side - so list with elements exists
             return_dict[parts[0]] = parts[1].split(",")     # write info onto return dict with elements of signal as key
         #print(return_dict)
+        print("# data import %s succesfull" % file_path)
         return return_dict
 
 
 def plotSignal(sig_ele, sig_noi):
-    stages = len(sig_ele["amplitudes"])
-    fig, ax = plt.subplots(figsize=(10, 5))
-    t = sig_noi[:,0]
+    """Calculates two sgnals one with noise and one pure The calculated signals will be plot in a single figure and saved as png
+    """
+    stages = len(sig_ele["amplitudes"])     # how many different signals will be calculated
+    fig, ax = plt.subplots(figsize=(10, 5)) # create the plot objects
+    t = sig_noi[:,0]                        # the time in ms from the sig_noise txt
+
+    # create so many plots and signals according to stages
     for i in range(0,stages,1):
-        print("Calc Signal N {}\n-------------".format(i))
+        print("- Calc signal and noisy signal Nr {} ...".format(i))
+
         A = float(sig_ele["amplitudes"][i])
         f = float(sig_ele["frequencies"][i])
         phi = float(sig_ele["phases"][i])
         linecolor = sig_ele["colors"][i]
-        calc_signal = A * numpy.sin(2 * numpy.pi * f * t + phi * (numpy.pi/180))
 
-        ax.plot(t, calc_signal, label="A = {}, f = {} Hz, phi = {} deg".format(A,f,phi))
+        calc_signal = A * numpy.sin(2 * numpy.pi * f * t + phi * (numpy.pi/180))                    # signal without noise
+        calc_noise = A * numpy.sin(2 * numpy.pi * f * t + phi * (numpy.pi / 180))+sig_noi[:,i+1]    # signal with noise
+
+        ax.set_xlabel("Time [s]")
+        ax.set_xlim((0,1))
+        ax.grid(True)
+        ax.plot(t, calc_signal, label="A = {}, f = {} Hz, phi = {} deg".format(A, f, phi), color=linecolor)
+        ax.plot(t, calc_noise, marker='o', color=linecolor, linewidth=0)
+
     plt.legend()
     plt.show()
+    print("- save fig signals.png")
+    fig.savefig("signals.png", dpi=300, format="png")
 
 if __name__ == "__main__":
 
+    print("ex01Signals\nPaul Arzberger\n=======================================\n")
+
+    #readout the signal noise and time info
+    signal_noise = numpy.loadtxt("noise.txt")
+    print("data import %s succesfull" % "noise.txt")
+
+    # read out the signal infos
     signal_elements = readSignals("signals.txt")
 
-    signal_noise = numpy.loadtxt("noise.txt")
-
+    # calculate and plot the signals
     plotSignal(signal_elements, signal_noise)
-
-
-
-
-
-
 
     print("Programm ENDE")
