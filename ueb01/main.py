f613d19525c88c6c5484d180d1c90e1184140970
Paul <peru77@gmx.at>
2019-04-05 21:58:43 +0200

    update file system

--- ueb01/main.py
+++ ueb01/main.py
@@ -1,5 +1,182 @@
+# Paul Arzberger
+# 00311430
+# Informatics 2 - SS19
+# Gruppe A - Programm 1: Drohnengetragenes Laserscanning
+
 import datetime
-from .Epochs import Epochs
+# =====================================================================================
+# Class Definitions
+# =====================================================================================
+class Epoch:
+    """
+    class Epoch contains the time attribute of type datetime and is the base class for the classes
+    PositionEpoch and PolarEpoch
+    """
+    # Constructor
+    def __init__(self, time, epoch_type="", verbose_input=False):
+        """
+        creates an instance of class Epoch and assignes the instance attribute time a datetime-value
+        :param time: of type datetime
+        :param epoch_type: string - that is handed to the screenprint and defines which of the inheriting classes is created.
+                                    if the epoch_type is Empty a default empty string is handed to the print function
+        :param verbose_input: True/False - will be set at the attribute self.__verbose and takes influence the information that
+                                    gets screened during the instantiation and deleting process of the class Epoch and its heritating
+                                    subclasses
+        """
+        if isinstance(time, datetime.datetime):
+
+            #print("  type: ", type(time))
+            self.__time = time              # has to be of type datetime
+            self.__verbose = verbose_input  # bool True or False - takes influence if info is screened during instantiation or deleting prcoess
+
+            # if verbose is set True the instantiation information will be printed to screen
+            if self.__verbose:
+                print("+ Creating new {}Epoch instance with time: {}".format(epoch_type, time))
+        else:
+            raise ValueError("!! The input time is not of type datetime.datetime !!")
+
+    # string representation if you print the instance on the screen
+    def __str__(self):
+        return "Timeepoch {}".format(self.time)
+
+    def __del__(self):
+        if self.__verbose:
+            print("- DELETE ALLLLL ")
+
+    # make the private attribute accessible for the public as a property
+    @property
+    def time(self):
+        return self.__time
+
+    @time.setter
+    def time(self, time):
+        self.__time = time
+
+    # property getter method to access verbose in the classes that inherrit from Epoch
+    @property
+    def verbose(self):
+        return self.__verbose
+
+    @verbose.setter
+    def verbose(self, bool_val):
+        self.__verbose = bool_val
+
+
+class PositionEpoch(Epoch):
+
+    #Constructor
+    def __init__(self, time, x=0., y=0., z=0., verbose_input=False):
+        super().__init__(time, "Position", verbose_input)
+
+        try:
+            if isinstance(float(x), float) and isinstance(float(y), float) and isinstance(float(z), float):
+                self.__x = x
+                self.__y = y
+                self.__z = z
+
+                if self.verbose:
+                    print("  x={} y={} z={}\n".format(self.__x, self.__y, self.__z))
+
+        except ValueError:
+            print("!!ERROR Constructor: \n  one of he inputparameters x: {} - {} - y: {} - {} - z: {} - {} is not useable\n"
+                             "  input must be a number or at least typecastable to a number"
+                             .format(x, type(x), y, type(y), z, type(z)))
+            # delete instance if input parameters are wrong
+            del self
+
+    # PROPERTIES
+    # ---------------------------
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def z(self):
+        return self.__z
+
+    @z.setter
+    def z(self, value):
+        self.__z = value
+
+    # string representation of the instance
+    def __str__(self):
+        return "PositionEpoch Instance {}\nx={} y={} z={}\n".format(self.time, self.__x, self.__y, self.__z)
+
+    # if verbose in the base class is true - a message will be printed out when the instance DIES
+    def __del__(self):
+        if self.verbose:
+            print("- Delete PositionEpoch Instance {}\n  x={} y={} z={}".format(self.time, self.__x, self.__y, self.__z))
+
+
+class PolarEpoch(Epoch):
+
+    # constructor
+    def __init__(self, time, distance=0., zenith=0., azimuth=0., verbose_input=False):
+        super().__init__(time, "Polar", verbose_input)
+        try:
+            if float(distance) and float(zenith) and float(azimuth):
+                self.__distance = distance
+                self.__zenith = zenith
+                self.__azimuth = azimuth
+
+                if self.verbose:
+                    print("  distance={} zenith={} azimuth={}".format(self.__distance, self.__zenith, self.__azimuth))
+
+        except ValueError:
+            print("!!ERROR Constructor: \n  one of he inputparameters distance: {} - {} - zenith: {} - {} - azimuth: {} - {} is not useable\n"
+                             "  input must be a number or at least typecastable to a number"
+                             .format(distance, type(distance), zenith, type(zenith), azimuth, type(azimuth)))
+            # delete instance if input parameters are wrong
+            del self
+
+    # PROPERTIES
+    # ---------------------------
+
+    @property
+    def distance(self):
+        return self.__distance
+
+    @distance.setter
+    def distance(self, value):
+        self.__distance = value
+
+    @property
+    def zenith(self):
+        return self.__zenith
+
+    @zenith.setter
+    def zenith(self, value):
+        self.__zenith = value
+
+    @property
+    def azimuth(self):
+        return self.__azimuth
+
+    @azimuth.setter
+    def azimuth(self, value):
+        self.__azimuth = value
+
+    # string representation of the instance if you print the class to the screen
+    def __str__(self):
+        return "PolarEpoch Instance {}\n  distance={} zenith={} azimuth={}\n".format(self.time, self.__distance, self.__zenith, self.__azimuth)
+
+    def __del__(self):
+
+        # if verbose in the base class is true - a message will be printed out when the instance DIES
+        if self.verbose:
+            print("- Delete PolarEpoch Instance {}\n  distance={} zenith={} azimuth={}".format(self.time, self.__distance, self.__zenith, self.__azimuth))
+
+
 
 # =====================================================================================
 # Function listing
@@ -35,7 +212,7 @@ def read_obs_data(input_file_path, input_start_epoch_str, verbose=False):
             new_epoch = datetime.datetime.fromtimestamp(start_epoch_seconds + float(t_sec_str))
 
             # append each observation to the returnlist as type PolarEpoch
-            observation_list.append(Epochs.PolarEpoch(new_epoch, float(dist_m_str), float(zenit_rad_str), float(azimuth_rad_str), verbose))
+            observation_list.append(PolarEpoch(new_epoch, float(dist_m_str), float(zenit_rad_str), float(azimuth_rad_str), verbose))
 
             cou += 1
             if cou == 10:
